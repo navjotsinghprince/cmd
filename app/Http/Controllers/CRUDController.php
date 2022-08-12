2af852903f57d22d144cb38801fa2cc880217bff
navjotsingh <navjotsinghprince1@gmail.com>
2022-08-12 10:12:16 +0530

    crud

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -168,14 +168,14 @@ class CRUDController extends Controller
         })->get();
 
 
-        #Use Function With Query  (Return warnings of user)
+        #Use Function With Query  (Return Warnings Of User)
         $warningUser = User::whereIn('id', $warningUserID)->with('profile')
             ->withCount(['warning' => function ($q) {
                 $q->where('warning', '1');
             }])->get();
 
 
-        #Single Column With Multiple Where Conditions (Check user phone not empty or null)
+        #Single Column With Multiple Where Conditions (Check User Phone Not Empty Or Null)
         $user = Profile::where('user_id', $request->user_id)
             ->where(function ($query) {
                 $query->where('phone', '!=', "")
@@ -183,14 +183,14 @@ class CRUDController extends Controller
             })
             ->first();
 
-        #Fetch all Only Trashed Records
+        #Fetch All Only Trashed Records
         $books = Book::onlyTrashed()->with('payment', 'order')
             ->orderBy('created_at', 'desc')->paginate($RowsPerPage);
         if ($books->isEmpty()) {
             return $this->notFound("No Trashed Books Found");
         }
 
-        #Fetch All Normal and Trashed Records
+        #Fetch All Normal And Trashed Records
         $books = Book::withTrashed()->with('payment', 'order')
             ->orderBy('created_at', 'desc')->paginate($RowsPerPage);
         if ($books->isEmpty()) {
@@ -198,24 +198,153 @@ class CRUDController extends Controller
         }
 
 
-        #Fetch Records With Relationships(Mutilple tables related data) 
+        #Fetch Records With Relationships(Mutilple Tables Related Data) 
         $homedata = Book::with('payment', 'order')->orderBy('id', 'desc')->paginate($RowsPerPage);
         if ($homedata->isEmpty()) {
             return $this->notFound("Home data Not Found");
         }
 
-        #Fetch Orders And users/payment of Specified User with Matching book_id(forign key)
-        $orders = AddOrder::with('payment', 'users')->where('book_id', $request->uid)
+        #Fetch Orders And Users/Payment Of Specified User With Matching Book_Id(Forign Key)
+        $orders = Order::with('payment', 'users')->where('book_id', $request->uid)
             ->orderBy('id', 'desc')->paginate($RowsPerPage);
         if ($orders->isEmpty()) {
             return $this->notFound("Orders Not Found");
         }
 
-        #Ignore(Columns) user phone number where id=1(Search Another People Phone Number Not User_id =1)
+        #Ignore(Columns) User Phone Number Where Id=1(Search Another People Phone Number Not User_Id =1)
         $Exists = Profile::where('user_id', '!=', 1)->where('phone', $phone)->count();
 
 
-        //WORKING WITH DATES
+        #Date Fetch Record With Single Date
+        $order = Order::with('users', 'payment')->where('book_id', $request->book_id)
+            ->whereDate('next_order', '=', $request->date)
+            ->orderBy('next_order', 'desc')
+            ->paginate($RowsPerPage);
+        if ($order->isEmpty()) {
+            return $this->notFound("Orders Not Found");
+        }
+
+        #Date Fetch Search Record Of Specified User With Date Id/From/To/Postage
+        #$from = date('2022-08-20');
+        #$to = date('2022-08-26');
+        #return records will be 21,22,23,24,25
+        $order = Order::with('payment', 'users')
+            ->whereBetween('next_order', [$request->from, $request->to])
+            ->where('book_id', $request->uid)
+            ->where('payment', $request->payment)
+            ->orderBy('next_order', 'desc')
+            ->paginate($RowsPerPage);
+        if ($order->isEmpty()) {
+            return $this->notFound("Order Not Found");
+        }
+
+
+        #Date Fetch Record With single date and Between Two Dates(both)
+        //or Where added For fetch 1 (single)Date record
+        $order = Order::with('users', 'payment')
+            ->whereBetween('next_order', [$request->from, $request->to])
+            ->orWhere('next_order', '=', $request->from)
+            ->where('payment', $request->payment)
+            ->orderBy('next_order', 'desc')
+            ->paginate($RowsPerPage);
+        if ($order->isEmpty()) {
+            return $this->notFound("Order Not Found");
+        }
+
+
+        #Fetch Record With Search Term
+        $homedata = Book::with('payment', 'order')
+            ->where('email', 'like', "%$search_term%")
+            ->orWhere('name', 'like', "%$search_term%")
+            ->orderBy('id', 'desc')->paginate(10);
+        if ($homedata->isEmpty()) {
+            return $this->notFound("No Home data Found");
+        }
+
+
+        #Fetch Records With Key Values Paires(Shipping Methods Title(Text) And Id(Value))
+        $cards = Shipping::orderBy('id', 'asc')->get()->toArray();
+        $p_options = [];
+        foreach ($cards as $key => $value) {
+            //echo $value["id"]." ".$value["title"];
+            $collect["text"] = $value["title"];
+            $collect["value"] = $value["id"];
+            array_push($p_options, $collect);
+        }
+        if (!$p_options) {
+            return $this->notFound("Postage Options Not Found");
+        } else {
+            return $this->actionSuccess("Home data Found Successfully", $p_options);
+        }
+
+
+        #Fetch Records With Collection Key Values Pairs(General Settings)
+        $collection = GeneralSettings::all();
+        $collection = $collection->keyBy('secret_key');
+        $response = new \stdClass();
+
+        $response->facebook  =  $collection->get('facebook_link')->value;
+        $response->github =  $collection->get('github_link')->value;
+        $response->linkedIn =  $collection->get('linkedin_link')->value;
+
+        if ($collection->isEmpty()) {
+            return $this->notFound("No data Found");
+        } else {
+            return $this->actionSuccess('Data Fetched', $response);
+        }
+    }
+
+    
+    /**
+     * DELETE 
+     * This will be used to delete the data on the database 🙂
+     */
+    public function DELETE(Request $request)
+    {
+
+        #Soft Delete(Moved To Trash Folder)
+        $book = Book::where('id', $request->user_id)->delete();
+        if ($book) {
+            return $this->actionSuccess("Moved to Trash");
+        }
+
+        #Restore(Restore The Trashed Record)
+        $book = Book::onlyTrashed()->where('id', $request->user_id)->restore();
+        if ($book) {
+            return $this->actionSuccess("User Restored");
+        }
+
+        #Force Delete(Parmanent Deletes The User)
+        $book = Book::onlyTrashed()->where('id', $request->user_id)->forceDelete();
+        if ($book) {
+            return $this->actionSuccess("User Deleted Parmanantly");
+        }
+    }
+
+
+    /**
+     * ADVANCED QUERIES 
+     * These are advanced queries on laravel 🙂
+     */
+    public function ADVANCED_QUERIES(Request $request)
+    {
+        $second_last_record=Transition::orderBy('created_at', 'desc')->skip(1)->take(1)->get(); #second last record
+
+        $get_all_count=CountryModel::get()->count(); #get all count
+
+        $get_last_updated_record=CountryModel::latest('updated_at')->first(); #get last updated record
+
+        $useremail = User::where('email', $request->email)->whereNotIn('email', ['navjot@gmail.com'])->first(); #where not in email example
+
+        $result = User::where('id', $request->user_id)->pluck('email')->first(); #only single column value
+
+        $PrimarySet = UserBankAccount::query()->update(['is_primary' => 0]);  #update all rows 
+
+        $last_reply = TicketConversationModel::where('ticket_id', $value->id)->orderBy('id', 'DESC')->latest('created_at')->pluck('created_at')->first(); #get last row reply date
+
+        $check['user_id'] = $user_id;
+        $data["name"] = "Prince Ferozepuria";
+        User::updateOrCreate($check, $data); #update create with check
 
     }
 }
